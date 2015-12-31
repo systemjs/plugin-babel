a026eea93373d807eba3022050ef4f722dbdff59
guybedford <guybedford@gmail.com>
2015-12-31 19:41:50 +0200

    build is working!

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd build-babel
 jspm install --quick --log err
 jspm build systemjs-babel.js ../systemjs-babel-browser.js --format amd --skip-source-maps --minify --log ok
-jspm build systemjs-babel.js ../systemjs-babel-node.js --format amd --node --skip-source-maps --minify --log ok
+jspm build systemjs-babel.js ../systemjs-babel-node.js --format amd --node --skip-source-maps --log ok
 jspm run systemjs-build-babel-helpers.js > ../babel-helpers.js
 cd ..
