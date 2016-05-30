8a7cc52a4a7fa404741ff5fb4fce616fcdb4f779
Douglas Duteil <douglasduteil@gmail.com>
2016-05-30 18:11:44 +0200

    feat(test): add raw browser testing

--- test/index.js
+++ test/index.js
@@ -3,26 +3,12 @@
 var test = require('tape');
 var SystemJS = require('systemjs');
 
-test('systemjs-plugin-babel', function (t) {
-  t.plan(1);
-
-  // given
-  System.config({
-    map: {
-      'plugin-babel': './plugin-babel.js',
-      'systemjs-babel-build': './systemjs-babel-node.js'
-    },
-    transpiler: 'plugin-babel'
-  });
-
-  // when
-  System.import('test/testing-code.js')
-
-  // then
-  .then(function(m) {
-    return m.p().then(function (message) {
-      t.equal(message, 'Rocks :)');
-    });
-  })
-  .then(t.end, t.end);
+System.config({
+  map: {
+    'plugin-babel': './plugin-babel.js',
+    'systemjs-babel-build': './systemjs-babel-node.js'
+  },
+  transpiler: 'plugin-babel'
 });
+
+require('./scenarios/index.js');
