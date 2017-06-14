c8a1409abd78d328c6b4b569533c7cb9fc8254df
guybedford <guybedford@gmail.com>
2017-06-14 13:28:36 +0200

    fix up sfx build

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ cd $BUILD_DIR
 cp -r jspm_packages_override/* jspm_packages
 
 jspm build systemjs-babel.js ../systemjs-babel-browser.js --format amd --skip-source-maps --skip-rollup --minify --log ok
-jspm build systemjs-babel.js ../systemjs-babel-node.js --format amd --node --skip-source-maps --skip-rollup --log ok
+jspm build systemjs-babel.js ../systemjs-babel-node.js --format amd --node --skip-source-maps --skip-rollup --log ok 
 jspm run systemjs-build-babel-helpers.js > ../babel-helpers.js
 jspm run systemjs-build-modular-babel-helpers.js
-(
-  echo "export default (function(module) {" ;
-  cat jspm_packages/npm/regenerator-runtime@$REGENERATOR_VERSION/runtime.js
-  echo "return module.exports; })({exports:{}});"
-) > ../regenerator-runtime.js
+# (
+#  echo "export default (function(module) {" ;
+#  cat jspm_packages/npm/regenerator-runtime@$REGENERATOR_VERSION/runtime.js
+#  echo "return module.exports; })({exports:{}});"
+# ) > ../regenerator-runtime.js
 cd ..
